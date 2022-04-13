ed003014cddecc2cb201eb010fd723e17ab8aeef
wingram1 <waingram96@gmail.com>
2022-04-13 17:07:58 -0500

    comments and replies

---
+++ models/Pizza.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String,
+    },
+    createdBy: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    size: {
+      type: String,
+      default: "Large",
+    },
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.reduce(
+    (total, comment) => total + comment.replies.length + 1,
+    0
+  );
+});
+
+// create Pizza model using the PizzaSchema
+const Pizza = model("Pizza", PizzaSchema);
+
+// export the Pizza model
+module.exports = Pizza;
